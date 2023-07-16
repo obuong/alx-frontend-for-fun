d0f97406dca8f88049a65a140184e5d9675d4489
Esau Obuong <iobuong2021@gmail.com>
2023-07-17 01:50:31 +0300

    task2

--- markdown2html.py
+++ markdown2html.py
@@ -22,6 +22,14 @@ with open(markdown_file, 'r') as file:
 # Parse headings syntax and generate HTML
 html_content = re.sub(r'^(#{1,6})\s+(.+)$', r'<\1>\2</\1>', markdown_content, flags=re.MULTILINE)
 
+# Parse unordered listing syntax and generate HTML
+html_content = re.sub(r'^-\s+(.+)$', r'<li>\1</li>', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'(?<=</h[1-6]>)(.*)(?=\n<ul>)', r'\1\n', html_content, flags=re.DOTALL)
+html_content = re.sub(r'(?<=</ul>)(.*)(?=<h[1-6]>)', r'\n\1', html_content, flags=re.DOTALL)
+html_content = re.sub(r'^\s*<h[1-6]>', r'\n<\g<0>', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'</h[1-6]>\s*$', r'\g<0>\n', html_content, flags=re.MULTILINE)
+html_content = re.sub(r'(?<=</ul>)\s*$', r'\n', html_content)
+
 # Write HTML content to the output file
 with open(output_file, 'w') as file:
     file.write(html_content)
