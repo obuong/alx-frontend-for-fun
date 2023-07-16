06fdb54174b3d26a8393cb9f8979adbd39fe4344
Esau Obuong <iobuong2021@gmail.com>
2023-07-17 02:23:37 +0300

    commit

--- markdown2html.py
+++ markdown2html.py
@@ -1,31 +1,71 @@
 #!/usr/bin/python3
 """
-Markdown to HTML Converter
+Markdown to HTML conversion 
 """
 
 import sys
-import os.path
-import markdown
+from os import path
+import re
+import hashlib
 
-def convert_markdown_to_html(markdown_file, output_file):
-    if not os.path.isfile(markdown_file):
-        print(f"Missing {markdown_file}", file=sys.stderr)
+def convert_md_to_html(md_file, html_file):
+    
+    if not path.exists(md_file):
+        print("Error: Markdown file not found")
         sys.exit(1)
-
-    with open(markdown_file, 'r') as file:
-        markdown_content = file.read()
-        html_content = markdown.markdown(markdown_content)
-
-    with open(output_file, 'w') as file:
-        file.write(html_content)
-
+        
+    html = []
+    
+    with open(md_file) as f:
+        lines = f.readlines()
+        
+    for line in lines:
+        if line.startswith("#"):
+            level = line.count("#") 
+            html.append(f"<h{level}>{line.lstrip('#').strip()}</h{level}>")
+            
+        elif line.startswith("-") or line.startswith("*"):
+            if line.startswith("-"):
+                tag = "ul"
+            else:
+                tag = "ol"
+                
+            if not html or "</li>" in html[-1]:
+                html.append(f"<{tag}>")
+                
+            html.append(f"<li>{line.lstrip('-').lstrip('*').strip()}</li>")
+            
+        elif line.startswith("```"):
+            continue
+            
+        else:
+            if not html or html[-1].startswith("<h") or html[-1].startswith("</li>"):
+                html.append("<p>")
+                
+            line = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", line)
+            line = re.sub(r"__(.+?)__", r"<em>\1</em>", line)
+            
+            line = re.sub(r"\[\[(.+?)\]\]", lambda m: hashlib.md5(m.group(1).encode()).hexdigest(), line)
+            line = re.sub(r"\(\((.+?)\)\)", lambda m: re.sub("[cC]", "", m.group(1)), line)
+            
+            if "<br/>" not in line:
+                line = line.replace("\n", "<br/>\n")
+                
+            html.append(line)
+            
+    for tag in ["ul", "ol", "p"]:
+        if html[-1].startswith(f"<{tag}>"):
+            html.append(f"</{tag}>")
+            
+    with open(html_file, "w") as f:
+        f.write("\n".join(html))
+        
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
-
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-    convert_markdown_to_html(markdown_file, output_file)
-
-    sys.exit(0)
+        
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+    
+    convert_md_to_html(md_file, html_file)
